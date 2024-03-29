151e6818a44fd30bfa7438958f74bdff21a56dee
zrefalogia <vuongquocphongv@gmail.com>
2024-03-29 22:00:50 +0700

    move page allocation into   allocproc

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,14 @@ found:
     return 0;
   }
 
+  if ((p->usyscall = (struct usyscall*) kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -160,6 +168,9 @@ freeproc(struct proc *p)
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
+  if (p->usyscall) {
+    kfree(p->usyscall);
+  }
   p->pagetable = 0;
   p->sz = 0;
   p->pid = 0;
@@ -202,16 +213,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-  struct usyscall *usyscall;
-  if ((usyscall = (struct usyscall *)kalloc()) == 0 ||
-    mappages(pagetable, USYSCALL, PGSIZE,
-            (uint64)(usyscall), PTE_R | PTE_U) < 0) {
+  
+  if (mappages(pagetable, USYSCALL, PGSIZE,
+            (uint64)(p->usyscall), PTE_R | PTE_U) < 0) {
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
     uvmunmap(pagetable, TRAPFRAME, 1, 0);
     uvmfree(pagetable, 0);
     return 0;
   }
-  usyscall->pid = p->pid;
 
   return pagetable;
 }
@@ -223,7 +232,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
-  uvmunmap(pagetable, USYSCALL, 1, 1);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
